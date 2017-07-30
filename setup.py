1a3172a2c2f36d214fc62ffcb5e67bf64a4e7f79
yaacov <kobi.zamir@gmail.com>
2017-07-30 10:48:11 +0300

    convert to python style

---
+++ setup.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+try:
+    long_description = open("README.rst").read()
+except IOError:
+    long_description = ""
+
+setup(
+    name="hebocr",
+    version="0.14.0",
+    description="hebOCR is a Hebrew character recognition library",
+    license="GPL",
+    author="yaacov",
+    packages=find_packages(),
+    install_requires=[],
+    long_description=long_description,
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+    ]
+)
